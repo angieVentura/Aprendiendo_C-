5ef1f83b63929d0a620cc1e81fa0de7d26558dc5
angieVentura <angiev.d.23@gmail.com>
2023-04-22 21:48:59 -0300

    Coordenas

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la temperatura en grados Celsius:");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine($"La temperatura {celsius}°C es equivalente a {fahrenheit}°F");
+            Console.ReadKey();
+        }
+    }
+}
