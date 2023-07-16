9b3a29cd2f5c2bb0735f624e0277f8d93ca14650
angieVentura <angiev.d.23@gmail.com>
2023-07-16 16:54:10 -0300

    Subo visor

---
+++ VisorForm/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.porcentaje = new System.Windows.Forms.Label();
+            this.Eliminar = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.btnMas = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.rotar = new System.Windows.Forms.Button();
+            this.pictureImg = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.pictureImg);
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 409);
+            this.panel1.TabIndex = 22;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(28, 12);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 26;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click_2);
+            // 
+            // porcentaje
+            // 
+            this.porcentaje.AutoSize = true;
+            this.porcentaje.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.porcentaje.Location = new System.Drawing.Point(747, 17);
+            this.porcentaje.Name = "porcentaje";
+            this.porcentaje.Size = new System.Drawing.Size(0, 13);
+            this.porcentaje.TabIndex = 25;
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.BackColor = System.Drawing.Color.Transparent;
+            this.Eliminar.Location = new System.Drawing.Point(109, 12);
+            this.Eliminar.Name = "Eliminar";
+            this.Eliminar.Size = new System.Drawing.Size(75, 23);
+            this.Eliminar.TabIndex = 22;
+            this.Eliminar.Text = "Eliminar";
+            this.Eliminar.UseVisualStyleBackColor = false;
+            this.Eliminar.Click += new System.EventHandler(this.Eliminar_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Location = new System.Drawing.Point(716, 12);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(25, 23);
+            this.btnMenos.TabIndex = 24;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click_1);
+            // 
+            // btnMas
+            // 
+            this.btnMas.Location = new System.Drawing.Point(685, 12);
+            this.btnMas.Name = "btnMas";
+            this.btnMas.Size = new System.Drawing.Size(25, 23);
+            this.btnMas.TabIndex = 23;
+            this.btnMas.Text = "+";
+            this.btnMas.UseVisualStyleBackColor = true;
+            this.btnMas.Click += new System.EventHandler(this.btnMas_Click_1);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(658, 41);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(132, 377);
+            this.flowLayoutPanel1.TabIndex = 28;
+            // 
+            // rotar
+            // 
+            this.rotar.BackColor = System.Drawing.Color.Transparent;
+            this.rotar.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.pngwing_com__1_;
+            this.rotar.Image = global::WindowsFormsApp1.Properties.Resources.pngwing1;
+            this.rotar.Location = new System.Drawing.Point(654, 12);
+            this.rotar.Name = "rotar";
+            this.rotar.Size = new System.Drawing.Size(25, 23);
+            this.rotar.TabIndex = 29;
+            this.rotar.Text = "⟲";
+            this.rotar.UseVisualStyleBackColor = false;
+            this.rotar.Click += new System.EventHandler(this.rotar_Click);
+            // 
+            // pictureImg
+            // 
+            this.pictureImg.BackColor = System.Drawing.Color.Transparent;
+            this.pictureImg.Location = new System.Drawing.Point(34, 29);
+            this.pictureImg.Name = "pictureImg";
+            this.pictureImg.Size = new System.Drawing.Size(554, 348);
+            this.pictureImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureImg.TabIndex = 28;
+            this.pictureImg.TabStop = false;
+            this.pictureImg.Click += new System.EventHandler(this.pictureImg_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rotar);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.porcentaje);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnMas);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.Eliminar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Label porcentaje;
+        private System.Windows.Forms.Button Eliminar;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.Button btnMas;
+        private System.Windows.Forms.PictureBox pictureImg;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button rotar;
+    }
+}
+
---
+++ VisorForm/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<Image> originalImages = new List<Image>();
+        private int zoomPercentage;
+        private int cantSum;
+        private int cantRes;
+        private int imgId;
+        private int rote = 0; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click_2(object sender, EventArgs e)
+        {
+            OpenFileDialog Img = new OpenFileDialog();
+            Img.Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.gif;*.bmp";
+            Img.Multiselect = true;
+            if (Img.ShowDialog() == DialogResult.OK)
+            {
+
+                foreach (string ruta in Img.FileNames)
+                {
+                    Image image = Image.FromFile(ruta);
+
+                    Size nuevo = Proporcional(image.Width, image.Height, 554, 348);
+
+                    originalImages.Add(new Bitmap(image, nuevo));
+                }
+                CrearPictureBoxEnPanel();
+
+                porcentaje.Text = $"100%";
+            }
+        }
+        private Size Proporcional(int originalWidth, int originalHeight, int maxWidth, int maxHeight)
+        {
+            double widthRatio = (double)maxWidth / originalWidth;
+            double heightRatio = (double)maxHeight / originalHeight;
+            double ratio = Math.Min(widthRatio, heightRatio);
+
+            int newWidth = (int)(originalWidth * ratio);
+            int newHeight = (int)(originalHeight * ratio);
+
+            return new Size(newWidth, newHeight);
+        }
+
+
+        private void CrearPictureBoxEnPanel()
+        {
+            flowLayoutPanel1.Controls.Clear();
+            if (originalImages.Count > 0)
+            {
+                foreach (Image img in originalImages)
+                {
+                    PictureBox pictureBox = new PictureBox();
+
+                    pictureBox.Size = new Size(100, 100); 
+                    pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    pictureBox.BackColor = Color.FromArgb(100, 100, 100);
+                    pictureBox.Image = img;
+
+                    flowLayoutPanel1.Controls.Add(pictureBox);
+
+                    pictureBox.Click += PictureBox_Click;
+                }
+            }
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            // Verificar que el objeto que generó el evento es un PictureBox
+            if (sender is PictureBox pictureBox)
+            {
+                porcentaje.Text = $"100%";
+                rote = 0;
+                pictureImg.Size = new Size(554, 348);
+                pictureImg.Location = new Point((panel1.Width - pictureImg.Width) / 2, (panel1.Height - pictureImg.Height) / 2);
+                zoomPercentage = 100;
+                cantSum = 6;
+                cantRes = 2;
+                int newWidth = pictureBox.Image.Width <= pictureBox.Image.Height ? (pictureBox.Image.Width * pictureImg.Height) / pictureBox.Image.Height : pictureImg.Width;
+                int newHeight = pictureBox.Image.Width > pictureBox.Image.Height ? (pictureBox.Image.Height * pictureImg.Width) / pictureBox.Image.Width : pictureImg.Height;
+                Image resizedImage = pictureBox.Image.GetThumbnailImage(newWidth, newHeight, null, IntPtr.Zero);
+                pictureImg.Image = resizedImage;
+
+                imgId = flowLayoutPanel1.Controls.GetChildIndex(pictureBox);
+            }
+        }
+
+        private void AplicarZoom()
+        {
+            if (originalImages.Count > 0)
+            {
+                int newWidth = (originalImages[imgId].Width * zoomPercentage) / 100;
+                int newHeight = (originalImages[imgId].Height * zoomPercentage) / 100;
+                Image resizedImage = new Bitmap(originalImages[imgId], newWidth, newHeight);
+
+                pictureImg.Image = resizedImage;
+                pictureImg.Size = new Size(newWidth, newHeight);
+                //pictureImg.Location = new Point((panel1.Width - newWidth) / 2, (panel1.Height - newHeight) / 2);
+                porcentaje.Text = $"{zoomPercentage}%";
+            }
+        }
+
+        private void btnMas_Click_1(object sender, EventArgs e)
+        {
+            if ((cantSum > 0 && cantRes < 8) && originalImages.Count > 0)
+            {
+                zoomPercentage += 10;
+                AplicarZoom();
+                cantSum--;
+                cantRes++;
+            }
+        }
+
+        private void btnMenos_Click_1(object sender, EventArgs e)
+        {
+
+            if ((cantRes >= 2 && cantRes <= 8) && originalImages.Count > 0)
+            {
+                zoomPercentage -= 10;
+                AplicarZoom();
+                cantSum++;
+                cantRes--;
+            }
+        }
+
+        private void Eliminar_Click(object sender, EventArgs e)
+        {
+            if (pictureImg.Image != null && originalImages.Count > 0)
+            {
+                originalImages.RemoveAt(imgId);
+                CrearPictureBoxEnPanel();
+                pictureImg.Image.Dispose();
+                pictureImg.Image = null;
+                porcentaje.Text = string.Empty;
+            }
+        }
+
+        private void rotar_Click(object sender, EventArgs e)
+        {
+            if (pictureImg.Image != null)
+            {
+                rote++;
+                int newWidth = (originalImages[imgId].Width * zoomPercentage) / 100;
+                int newHeight = (originalImages[imgId].Height * zoomPercentage) / 100;
+
+                if (rote % 2 == 0)
+                    pictureImg.Size = new Size(newWidth, newHeight);
+                else
+                {
+                    pictureImg.Size = new Size(newHeight, newWidth);
+                }
+
+                Image Image = pictureImg.Image;
+
+                Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+                pictureImg.Image = Image;
+            }
+        }
+
+        private void pictureImg_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureImg_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
