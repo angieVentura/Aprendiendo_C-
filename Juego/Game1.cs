c00b25937d4d5b8e4dc89c64a624d7d88caa1045
angieVentura <angiev.d.23@gmail.com>
2023-09-07 22:50:50 -0300

    Subo imagenes y logica para hacer el piso

---
+++ Juego/Game1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Juego
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D piso;
+        int[,] escenario;
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            escenario = new int[10, 20];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (i == 9) 
+                        escenario[i, j] = 1;
+                    else
+                        escenario[i, j] = 0;
+                }
+            }
+
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            piso = Content.Load<Texture2D>("Terrain Tileset");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            // Dibujando pisososoosos
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (escenario[i, j] == 1) 
+                    {
+                        Rectangle pisoNieve = new Rectangle(0, 208, 48, 48);
+                        _spriteBatch.Draw(piso, new Vector2(j * 48, i * 48), pisoNieve, Color.White);
+                    }
+                }
+            }
+
+            _spriteBatch.End(); 
+
+            base.Draw(gameTime);
+        }
+    }
+}
