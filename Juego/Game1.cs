13bdf1f10732a39df4df4b5e285f49d27ef16fde
angieVentura <angiev.d.23@gmail.com>
2023-09-08 15:49:37 -0300

    Subo piso, fondo y sprites

--- Juego/Game1.cs
+++ Juego/Game1.cs
@@ -8,7 +8,7 @@ namespace Juego
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D piso;
+        Texture2D piso, fondoC1, fondoC2, fondoC3, fondoC4;
         int[,] escenario;
         
         public Game1()
@@ -17,11 +17,11 @@ namespace Juego
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            escenario = new int[10, 20];
+            escenario = new int[10, 25];
 
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j < 25; j++)
                 {
                     if (i == 9) 
                         escenario[i, j] = 1;
@@ -43,10 +43,14 @@ namespace Juego
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+
             // TODO: use this.Content to load your game content here
 
             piso = Content.Load<Texture2D>("Terrain Tileset");
-
+            fondoC1 = Content.Load<Texture2D>("sky_lightened");
+            fondoC2 = Content.Load<Texture2D>("glacial_mountains");
+            fondoC3 = Content.Load<Texture2D>("clouds_mg_2");
+            fondoC4 = Content.Load<Texture2D>("clouds_mg_1");
         }
 
         protected override void Update(GameTime gameTime)
@@ -62,18 +66,30 @@ namespace Juego
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            Vector2 escalaPiso = new Vector2(2.0f, 2.0f);
+
+            Vector2 escalaFondos = new Vector2(2.11f, 2.11f);
+            
+ 
 
             _spriteBatch.Begin();
+            Rectangle fondoRec = new Rectangle(0, 0, 380, 225);
+            _spriteBatch.Draw(fondoC1, new Vector2( 0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
+            _spriteBatch.Draw(fondoC2, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
+            _spriteBatch.Draw(fondoC3, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
+            _spriteBatch.Draw(fondoC4, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
+
 
             // Dibujando pisososoosos
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 20; j++)
+                for (int j = 0; j < 25; j++)
                 {
                     if (escenario[i, j] == 1) 
                     {
-                        Rectangle pisoNieve = new Rectangle(0, 208, 48, 48);
-                        _spriteBatch.Draw(piso, new Vector2(j * 48, i * 48), pisoNieve, Color.White);
+                        Rectangle pisoNieve = new Rectangle(20, 208, 16, 16);
+                        _spriteBatch.Draw(piso, new Vector2(j * 32, i * 50), pisoNieve, Color.White, 0f, Vector2.Zero, escalaPiso, SpriteEffects.None, 0f);
+                        
                     }
                 }
             }
