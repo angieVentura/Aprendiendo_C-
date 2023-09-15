9ae532540c151047e54fafba38217d45c457447e
angieVentura <angiev.d.23@gmail.com>
2023-09-15 12:38:23 -0300

    Subo logica inicial de animaciones

--- Juego/Game1.cs
+++ Juego/Game1.cs
@@ -1,21 +1,69 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Linq;
 
 namespace Juego
 {
+    public class Animation
+    {
+        private Texture2D texture;
+        private int frameWidth;
+        private int frameHeight;
+        private int frameCount;
+        private int currentFrame;
+        private float frameTime;
+        private float timer;
+        private int row;
+
+        public Animation(Texture2D texture, int frameWidth, int frameHeight, int frameCount, float frameTime, int row)
+        {
+            this.texture = texture;
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+            this.frameCount = frameCount;
+            this.frameTime = frameTime;
+            this.timer = 0;
+            this.currentFrame = 0;
+            this.y = row;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer > frameTime)
+            {
+                currentFrame = (currentFrame + 1) % frameCount;
+                timer = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Color color, float scale)
+        {
+            Rectangle sourceRectangle = new Rectangle(currentFrame * frameWidth, row * frameHeight, frameWidth, frameHeight);
+            spriteBatch.Draw(texture, position, sourceRectangle, color, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        Texture2D piso, fondoC1, fondoC2, fondoC3, fondoC4, pinguinoSprites, pinguinoPrueba;
+        Texture2D piso, fondoC1, fondoC2, fondoC3, fondoC4, pinguinoSprites, pinguinoPrueba, FondoP;
         int[,] escenario;
-        float timer;
-        int threshold;
-        Rectangle[] pinguino;
-        byte previousAnimationIndex;
-        byte currentAnimationIndex;
+        Animation pinguinoAnimation;
 
+        public enum PinguinoAnimation
+        {
+            Caminar,
+            Saltar,
+            OtraAnimacion
+        }
+
+        PinguinoAnimation currentPinguinoAnimation;
+        Animation currentAnimation;
+        Animation caminarAnimation;
 
         public Game1()
         {
@@ -40,8 +88,6 @@ namespace Juego
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
@@ -49,24 +95,19 @@ namespace Juego
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            timer = 0;
-
-            threshold = 250;
-
-
-
-            previousAnimationIndex = 2;
-            currentAnimationIndex = 1;
-            // TODO: use this.Content to load your game content here
-
             piso = Content.Load<Texture2D>("Terrain Tileset");
             fondoC1 = Content.Load<Texture2D>("sky_lightened");
             fondoC2 = Content.Load<Texture2D>("glacial_mountains");
             fondoC3 = Content.Load<Texture2D>("clouds_mg_2");
             fondoC4 = Content.Load<Texture2D>("clouds_mg_1");
-          //  pinguinoSprites = Content.Load<Texture2D>("pinguinoCaminando-sheet");
-            pinguinoPrueba = Content.Load<Texture2D>("pinguinoCaminando1");
-       
+            pinguinoSprites = Content.Load<Texture2D>("pinguino/pinguino");
+            pinguinoPrueba = Content.Load<Texture2D>("pinguino/pinguino");
+            FondoP = Content.Load<Texture2D>("Fondo");
+            //pinguinoAnimation = new Animation(pinguinoSprites, 144, 155, 6, 100);
+            caminarAnimation = new Animation(pinguinoSprites,144, 155, 6, 100,0);
+            currentPinguinoAnimation = PinguinoAnimation.Caminar;
+            currentAnimation = caminarAnimation;
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -74,40 +115,19 @@ namespace Juego
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //pinguinoAnimation.Update(gameTime);
 
-            // Check if the timer has exceeded the threshold.
-            if (timer > threshold)
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
-                // If Alex is in the middle sprite of the animation.
-                if (currentAnimationIndex == 1)
-                {
-                    // If the previous animation was the left-side sprite, then the next animation should be the right-side sprite.
-                    if (previousAnimationIndex == 0)
-                    {
-                        currentAnimationIndex = 2;
-                    }
-                    else
-                    // If not, then the next animation should be the left-side sprite.
-                    {
-                        currentAnimationIndex = 0;
-                    }
-                    // Track the animation.
-                    previousAnimationIndex = currentAnimationIndex;
-                }
-                // If Alex was not in the middle sprite of the animation, he should return to the middle sprite.
-                else
-                {
-                    currentAnimationIndex = 1;
-                }
-                // Reset the timer.
-                timer = 0;
+                currentPinguinoAnimation = PinguinoAnimation.Caminar;
+                currentAnimation = caminarAnimation;
             }
-            // If the timer has not reached the threshold, then add the milliseconds that have past since the last Update() to the timer.
             else
             {
-                timer += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
             }
+
+            currentAnimation.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -115,48 +135,36 @@ namespace Juego
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            //Escena
             Vector2 escalaPiso = new Vector2(2.0f, 2.0f);
-
             Vector2 escalaFondos = new Vector2(2.11f, 2.11f);
-
-            Vector2 escalaPinguinos = new Vector2(2.5f, 2.5f);
- 
-
+            //Personajes
+            Vector2 escalaPinguinos = new Vector2(0.3f, 0.3f);
+            //Debug
+            Color backgroundColor = Color.Red;
 
             _spriteBatch.Begin();
+            
+            //Fondo
             Rectangle fondoRec = new Rectangle(0, 0, 380, 225);
             _spriteBatch.Draw(fondoC1, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
             _spriteBatch.Draw(fondoC2, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
             _spriteBatch.Draw(fondoC3, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
             _spriteBatch.Draw(fondoC4, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
 
+            // Dibujando pisososoosos jejejejejje
+            var positionsToDraw = from i in Enumerable.Range(0, 10) from j in Enumerable.Range(0, 25) where escenario[i, j] == 1 select new Vector2(j * 32, i * 50);
 
-            // Dibujando pisososoosos
-            for (int i = 0; i < 10; i++)
+            foreach (var position in positionsToDraw)
             {
-                for (int j = 0; j < 25; j++)
-                {
-                    if (escenario[i, j] == 1) 
-                    {
-                        Rectangle pisoNieve = new Rectangle(20, 208, 16, 16);
-                        _spriteBatch.Draw(piso, new Vector2(j * 32, i * 50), pisoNieve, Color.White, 0f, Vector2.Zero, escalaPiso, SpriteEffects.None, 0f);
-                        
-                    }
-                }
+                Rectangle pisoNieve = new Rectangle(20, 208, 16, 16);
+                _spriteBatch.Draw(piso, position, pisoNieve, Color.White, 0f, Vector2.Zero, escalaPiso, SpriteEffects.None, 0f);
             }
 
-            //Rectangle pinguinoRec = new Rectangle(20, 208, 16, 16);
-          /*  Rectangle[] pinguino = new Rectangle[6];
+            //Personajes
+            currentAnimation.Draw(_spriteBatch, new Vector2(10, 404), Color.White, 0.37f);
 
-            pinguino[0] = new Rectangle(0, 0, 16, 16);
-            pinguino[1] = new Rectangle(16, 0, 16, 16);
-            pinguino[2] = new Rectangle(32, 0, 16, 16);
-            pinguino[3] = new Rectangle(48, 0, 16, 16);
-            pinguino[4] = new Rectangle(64, 0, 16, 16);
-            pinguino[5] = new Rectangle(80, 0, 16, 16);
 
-            _spriteBatch.Draw(pinguinoSprites, new Vector2(10,395), pinguino[currentAnimationIndex], Color.White, 0f, Vector2.Zero, escalaPinguinos, SpriteEffects.None, 0f);
-            _spriteBatch.Draw(pinguinoPrueba, new Vector2(0, 0), new Rectangle(0, 0, 32, 32), Color.White, 0f, Vector2.Zero, new Vector2(2.5f, 2.5f), SpriteEffects.None, 0f);*/
             _spriteBatch.End(); 
 
             base.Draw(gameTime);
