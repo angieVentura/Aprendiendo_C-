fa6312a01cf036d39a3188b3e736c083accf855c
angieVentura <angiev.d.23@gmail.com>
2023-09-16 18:46:19 -0300

    Inicio logica de lista de objetos

--- Juego/Game1.cs
+++ Juego/Game1.cs
@@ -1,24 +1,20 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Juego
 {
     public class Animation
     {
         private Texture2D texture;
-        private int frameWidth;
-        private int frameHeight;
-        private int frameCount;
-        private int currentFrame;
-        private float frameTime;
-        private float timer;
-        private int row;
-        private bool move,right,up;
-        private Vector2 movimiento;
+        private int frameWidth,frameHeight, frameCount, currentFrame, row;
+        private float frameTime, timer;
+        private bool left;
 
-        public Animation(Texture2D texture, int frameWidth, int frameHeight, int frameCount, float frameTime, int row, bool move, bool right, bool up, Vector2 movimiento)
+        public Animation(Texture2D texture, int frameWidth, int frameHeight, int frameCount, float frameTime, int row, bool left)
         {
             this.texture = texture;
             this.frameWidth = frameWidth;
@@ -28,8 +24,7 @@ namespace Juego
             this.timer = 0;
             this.currentFrame = 0;
             this.row = row;
-            this.move = move;
-            this.movimiento = movimiento;
+            this.left = left;
             
         }
 
@@ -48,16 +43,31 @@ namespace Juego
         {
          
             Rectangle sourceRectangle = new Rectangle(currentFrame * frameWidth, row * frameHeight, frameWidth, frameHeight);
-            spriteBatch.Draw(texture, position , sourceRectangle, color, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, position , sourceRectangle, color, 0f, Vector2.Zero, scale, left ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0f);
+        }
+    }
+
+    public class Elemento
+    {
+        public string Tipo { get; set; }
+        public Vector2 Posicion { get; set; }
+        public Vector2 Tamano { get; set; }
+
+        public Elemento(string tipo, Vector2 posicion, Vector2 tamano)
+        {
+            Tipo = tipo;
+            Posicion = posicion;
+            Tamano = tamano;
         }
     }
 
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
+
         private SpriteBatch _spriteBatch;
         Texture2D piso, fondoC1, fondoC2, fondoC3, fondoC4, pinguinoSprites, pinguinoPrueba, FondoP;
-        int[,] escenario;
+        List<Elemento> elementos = new List<Elemento>();
         Vector2 posFotograma = new Vector2(5, 400);
 
         public enum PinguinoAnimation
@@ -71,36 +81,34 @@ namespace Juego
         Animation currentAnimation;
         Animation caminarAnimation;
         Animation waitAnimation;
-        bool desEnX;
-        bool desEnY;
-        SpriteEffects flipHorizontal = SpriteEffects.FlipHorizontally;
+        Animation caminarAnimationL;
 
         public Game1()
         {
             //pantalla de 800 x 600
             _graphics = new GraphicsDeviceManager(this);
 
+            this._graphics.PreferredBackBufferWidth = 800;
+            this._graphics.PreferredBackBufferHeight = 600;
+
             Content.RootDirectory = "Content";
+
+            _graphics.IsFullScreen= true;
+
             IsMouseVisible = true;
 
-            escenario = new int[10, 25];
-
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 25; j++)
-                {
-                    if (i == 9) 
-                        escenario[i, j] = 1;
-                    else
-                        escenario[i, j] = 0;
-                }
-            }
 
         }
 
         protected override void Initialize()
         {
             base.Initialize();
+
+            for (int i = 0)
+            {
+
+            }
+
         }
 
         protected override void LoadContent()
@@ -115,10 +123,9 @@ namespace Juego
             pinguinoSprites = Content.Load<Texture2D>("pinguino/pinguino");
             pinguinoPrueba = Content.Load<Texture2D>("pinguino/pinguino");
             FondoP = Content.Load<Texture2D>("Fondo");
-            caminarAnimation = new Animation(pinguinoSprites,144, 144, 6, 100, 0, true, desEnX, desEnY, new Vector2(5, 0));
-            waitAnimation = new Animation(pinguinoSprites, 144, 144, 4, 1000, 3, false, false, false, new Vector2(0,0));
-            currentPinguinoAnimation = PinguinoAnimation.Wait;
-            //currentAnimation = waitAnimation;
+            caminarAnimation = new Animation(pinguinoSprites,144, 144, 6, 100, 0, false);
+            waitAnimation = new Animation(pinguinoSprites, 144, 144, 4, 1000, 3, false);
+            caminarAnimationL = new Animation(pinguinoSprites, 144, 144, 6, 100, 0, true);
 
         }
 
@@ -130,14 +137,14 @@ namespace Juego
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
             {
                 currentPinguinoAnimation = PinguinoAnimation.Caminar;
-                
                 currentAnimation = caminarAnimation;
                 posFotograma.X += 1;
             }
             else if (Keyboard.GetState().IsKeyDown(Keys.Left))
             {
                 currentPinguinoAnimation = PinguinoAnimation.Caminar;
-
+                currentAnimation = caminarAnimationL;
+                posFotograma.X -= 1;
             }
             else
             {
@@ -162,7 +169,6 @@ namespace Juego
             Color backgroundColor = Color.Red;
 
             _spriteBatch.Begin();
-            
             //Fondo
             Rectangle fondoRec = new Rectangle(0, 0, 380, 225);
             _spriteBatch.Draw(fondoC1, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
@@ -170,19 +176,16 @@ namespace Juego
             _spriteBatch.Draw(fondoC3, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
             _spriteBatch.Draw(fondoC4, new Vector2(0, 0), fondoRec, Color.White, 0f, Vector2.Zero, escalaFondos, SpriteEffects.None, 0f);
 
-            // Dibujando pisososoosos jejejejejje
-            var positionsToDraw = from i in Enumerable.Range(0, 10) from j in Enumerable.Range(0, 25) where escenario[i, j] == 1 select new Vector2(j * 32, i * 50);
+            /*var positionsToDraw = from i in Enumerable.Range(0, 10) from j in Enumerable.Range(0, 25) where escenario[i, j] == 1 select new Vector2(j * 32, i * 50);
 
             foreach (var position in positionsToDraw)
             {
                 Rectangle pisoNieve = new Rectangle(20, 208, 16, 16);
                 _spriteBatch.Draw(piso, position, pisoNieve, Color.White, 0f, Vector2.Zero, escalaPiso, SpriteEffects.None, 0f);
-            }
+            }*/
 
             //Personajes
-            currentAnimation.Draw(_spriteBatch, posFotograma, null, Color.White, 0.37f);
-
-
+            currentAnimation.Draw(_spriteBatch, posFotograma,  Color.White, 0.37f );
             _spriteBatch.End(); 
 
             base.Draw(gameTime);
