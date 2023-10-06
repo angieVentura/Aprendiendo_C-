91edb53bbd3995db784f923e2ccd84ee8c8a697d
angieVentura <angiev.d.23@mail.com>
2023-10-06 14:57:02 -0300

    Subo prueba tarde ToT, no me di cuenta

---
+++ prueba06.10.23/prueba06.10.23/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace prueba06._10._23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ordenar = new System.Windows.Forms.Button();
+            this.borrar = new System.Windows.Forms.Button();
+            this.intercambiar = new System.Windows.Forms.Button();
+            this.interPrimeroUltimo = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ordenar
+            // 
+            this.ordenar.Location = new System.Drawing.Point(346, 118);
+            this.ordenar.Name = "ordenar";
+            this.ordenar.Size = new System.Drawing.Size(75, 23);
+            this.ordenar.TabIndex = 0;
+            this.ordenar.Text = "Ordenar";
+            this.ordenar.UseVisualStyleBackColor = true;
+            this.ordenar.Click += new System.EventHandler(this.ordenar_Click);
+            // 
+            // borrar
+            // 
+            this.borrar.Location = new System.Drawing.Point(346, 174);
+            this.borrar.Name = "borrar";
+            this.borrar.Size = new System.Drawing.Size(75, 23);
+            this.borrar.TabIndex = 1;
+            this.borrar.Text = "Borrar";
+            this.borrar.UseVisualStyleBackColor = true;
+            this.borrar.Click += new System.EventHandler(this.borrar_Click);
+            // 
+            // intercambiar
+            // 
+            this.intercambiar.Location = new System.Drawing.Point(346, 232);
+            this.intercambiar.Name = "intercambiar";
+            this.intercambiar.Size = new System.Drawing.Size(75, 23);
+            this.intercambiar.TabIndex = 2;
+            this.intercambiar.Text = "Intercambiar";
+            this.intercambiar.UseVisualStyleBackColor = true;
+            this.intercambiar.Click += new System.EventHandler(this.intercambiar_Click);
+            // 
+            // interPrimeroUltimo
+            // 
+            this.interPrimeroUltimo.Location = new System.Drawing.Point(346, 288);
+            this.interPrimeroUltimo.Name = "interPrimeroUltimo";
+            this.interPrimeroUltimo.Size = new System.Drawing.Size(75, 23);
+            this.interPrimeroUltimo.TabIndex = 3;
+            this.interPrimeroUltimo.Text = "Pri y ulti";
+            this.interPrimeroUltimo.UseVisualStyleBackColor = true;
+            this.interPrimeroUltimo.Click += new System.EventHandler(this.interPrimeroUltimo_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(61, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 329);
+            this.listBox1.TabIndex = 4;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(513, 56);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(202, 329);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.interPrimeroUltimo);
+            this.Controls.Add(this.intercambiar);
+            this.Controls.Add(this.borrar);
+            this.Controls.Add(this.ordenar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ordenar;
+        private System.Windows.Forms.Button borrar;
+        private System.Windows.Forms.Button intercambiar;
+        private System.Windows.Forms.Button interPrimeroUltimo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ prueba06.10.23/prueba06.10.23/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueba06._10._23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool inter = true;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Hola");
+            listBox1.Items.Add("Chau");
+            listBox1.Items.Add("React");
+            listBox1.Items.Add("Navidad");
+            listBox1.Items.Add("Dormir");
+            listBox1.Items.Add("Facturas");
+            listBox1.Items.Add("Computadoras");
+            listBox1.Items.Add("Goleada");
+            listBox1.Items.Add("Domingo");
+            listBox1.Items.Add("Exponer");
+
+            listBox2.Items.Add("Lunes");
+            listBox2.Items.Add("Efemerides");
+            listBox2.Items.Add("Cumpleaños");
+            listBox2.Items.Add("Empanadas");
+            listBox2.Items.Add("Matematica");
+            listBox2.Items.Add("Informes");
+            listBox2.Items.Add("Demanda");
+            listBox2.Items.Add("Oferta");
+            listBox2.Items.Add("Oferentes");
+            listBox2.Items.Add("Demandantes");
+
+        }
+
+        private void ordenar_Click(object sender, EventArgs e)
+        {
+            //Cuando se presiona en el primer boton ordenar los listados
+
+            listBox1.Sorted = true;
+            listBox2.Sorted = true;
+
+        }
+
+        private void borrar_Click(object sender, EventArgs e)
+        {
+            //Con el segundo boton borran todos los nombres que tengan hasta 5 letras
+
+
+            Borrar(listBox1);
+            Borrar(listBox2);
+
+        }
+
+        private void intercambiar_Click(object sender, EventArgs e)
+        {
+            //Con el tercer boton intercambiar los nombres que tengan hasta 5 letras
+
+            Intercambiar(listBox1, listBox2);
+
+        }
+
+        private void interPrimeroUltimo_Click(object sender, EventArgs e)
+        {
+            //Intercambiar el primer y ultimo elemento de ambas listas. Tienen que quedar en el primer lugar y en el ultimo
+
+            string[] nombresPri = { listBox1.Items[0].ToString(), listBox2.Items[0].ToString()};
+            string[] nombresUlt = { listBox1.Items[listBox1.Items.Count - 1].ToString(), listBox2.Items[listBox2.Items.Count - 1].ToString()};
+
+            listBox1.Items[0] = nombresPri[1];
+            listBox2.Items[0] = nombresPri[0];
+            listBox1.Items[listBox1.Items.Count - 1] = nombresUlt[1];
+            listBox2.Items[listBox2.Items.Count - 1] = nombresUlt[0];
+        }
+
+
+        public void Borrar(ListBox listBox) {
+            for (int i = listBox.Items.Count - 1; i > 0; i--)
+            {
+                if (listBox.Items[i].ToString().Length <= 5)
+                {
+                    listBox.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        public void Intercambiar(ListBox listBoxA, ListBox listBoxB)
+        {
+            List<string> listBoxA5 = new List<string>();
+            List<string> listBoxB5 = new List<string>();
+
+            for (int i = 0; i < listBoxA.Items.Count; i++)
+            {
+                if (listBoxA.Items[i].ToString().Length <= 5)
+                {
+                    listBoxA5.Add(listBoxA.Items[i].ToString());
+                } 
+            }
+            for (int i = 0; i < listBoxB.Items.Count; i++)
+            {
+                if (listBoxB.Items[i].ToString().Length <= 5)
+                {
+                    listBoxB5.Add(listBoxB.Items[i].ToString());
+                }
+            }
+
+            Borrar(listBoxB);
+            Borrar(listBoxA);
+
+            for (int i = 0; i < listBoxA5.Count; i++)
+            {
+                if (!Contiene(listBoxA5[i], listBoxB))
+                    listBoxB.Items.Add(listBoxA5[i].ToString());
+                                
+            }
+
+            for (int i = 0; i < listBoxB5.Count; i++)
+            {
+                if (!Contiene(listBoxB5[i], listBoxA))
+                    listBoxA.Items.Add(listBoxB5[i].ToString());
+            }
+
+        }
+
+        public bool Contiene(string palabra, ListBox listBox )
+        {
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                if (listBox.Items[i].ToString() == palabra)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
